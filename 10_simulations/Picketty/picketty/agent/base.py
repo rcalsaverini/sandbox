edb2d39907a5ee5f9af79e6aaee88b00f7adac8c
Rafael S. Calsaverini <rafael.calsaverini@gmail.com>
2024-05-07 21:12:27 -0300

    Update

--- 10_simulations/Picketty/picketty/agent/base.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-HOURS_PER_DAY = 24.0
-
-@dataclass
-class Agent:
-    wealth: float
-    needs: float
--- 10_simulations/Picketty/picketty/economy/base.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-from company.base import Job
-
-
-class Economy:
-    price: float
-    job_board: List[Job]
